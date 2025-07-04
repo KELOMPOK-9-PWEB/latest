4236dd871434eedafc176cf3546787d494ad3b76
Nazzww <nazwaaulyarahman2@gmail.com>
2025-07-04 20:09:05 +0700

    Create info_ukm.php

---
+++ info_ukm.php
@@ -0,0 +1,29 @@
+<?php include 'navbar.php'; ?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>Info UKM</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+  <h2 style="color:#003366; text-align:center;">INFO UKM UNIVERSITAS AHMAD DAHLAN</h2>
+  <p style="text-align:center; margin-bottom: 30px;">Berikut beberapa informasi terbaru dari Unit Kegiatan Mahasiswa (UKM) di UAD:</p>
+
+  <ul style="line-height: 1.8; font-size: 16px;">
+    <li><strong>[Rekrutmen]</strong> UKM Musik membuka pendaftaran anggota baru hingga <u>25 Juli 2025</u>. Pendaftaran bisa dilakukan melalui halaman <a href="form_daftar.php">daftar UKM</a>.</li>
+
+    <li><strong>[Pengumuman]</strong> Rapat koordinasi seluruh ketua UKM akan dilaksanakan pada <u>8 Juli 2025</u> pukul 13.00 WIB di Aula Kampus 4 UAD.</li>
+
+    <li><strong>[Prestasi]</strong> Selamat kepada UKM Basket UAD yang berhasil meraih Juara 2 dalam Turnamen Bola Basket Antar-Kampus se-DIY bulan Juni lalu!</li>
+
+    <li><strong>[Perubahan Jadwal]</strong> Latihan UKM Tari yang biasanya hari Sabtu dipindah ke <u>hari Minggu pukul 09.00</u> selama bulan Juli karena renovasi gedung seni.</li>
+
+    <li><strong>[Penghargaan]</strong> UKM Tari menjadi perwakilan UAD dalam Festival Seni Mahasiswa Nasional (FESMA) 2025.</li>
+  </ul>
+</div>
+
+</body>
+</html>
